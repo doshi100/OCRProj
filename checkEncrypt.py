84b4fa7f93dd4d6c03a1da54338fcd4bb9134e4c
idan <doshi_idan@yahoo.com>
2022-06-03 14:17:31 +0300

    commit 3/6/22

--- checkEncrypt.py
+++ checkEncrypt.py
@@ -8,32 +8,68 @@ from os import listdir
 from os.path import isfile, join
 from pytesseract import Output
 
+
+def cropPaste(sourceList, compList):
+    for dic in sourceList:
+        cropped_img = dic["img_obj"]
+        org_img = dic["org_img"]
+        img_height = np.shape(cropped_img)[0]
+        img_width = np.shape(cropped_img)[1]
+        org_img[dic["crop_coor"][1]:img_height, dic["crop_coor"][0]:img_width] = dic["img_obj"]
+        dic["org_img"] = org_img
+    for dic in compList:
+        cropped_img = dic["img_obj"]
+        org_img = dic["org_img"]
+        img_height = np.shape(cropped_img)[0] + dic["crop_coor"][0]
+        img_width = np.shape(cropped_img)[1] + dic["crop_coor"][1]
+        org_img[dic["crop_coor"][0]:img_height, dic["crop_coor"][1]:img_width] = dic["img_obj"]
+        dic["org_img"] = org_img
+
+
+# shows all the images that are in the compare and source img lists.
 def imgShow(source_photos, compare_photos):
     counter = 0
     for photo in source_photos:
-        cv2.imshow("{}".format(random.randint(1, 1000)), source_photos[counter]["img_obj"])
+        cv2.imshow("{}".format(random.randint(1, 1000)), source_photos[counter]["org_img"])
         # cv2.waitKey(0)
         counter += 1
     counter = 0
     for photo2 in compare_photos:
-        cv2.imshow("{}".format(random.randint(1, 1000)), compare_photos[counter]["img_obj"])
+        cv2.imshow("{}".format(random.randint(1, 1000)), compare_photos[counter]["org_img"])
         # cv2.waitKey(0)
         counter += 1
 
-
-def ImageToData(img_address, is_table):
+            #   (1) img location in memory
+            #   (2) bool set to true if image depict a table
+            #   (3) specify if the img was cropped and provide the original img
+            #   (4) specify the box coordinates of the cropped image (tuple)
+def ImageToData(img_address, is_table, orgImg=None, orgImg_coor=None):
     img_data = None
     if(is_table):
         img = cv2.imread(img_address)
         result = OCR_correct(img)
         img_data = pytesseract.image_to_data(result, output_type=Output.DICT, config='tessedit_char_whitelist=0123456789')
         img_data["img_obj"] = img
+        if(orgImg is not None):
+            org_img = cv2.imread(orgImg)
+            img_data["org_img"] = org_img
+            img_data["crop_coor"] = orgImg_coor
+        # if the photo wasn't cropped, put the original image into the org_img as well as img_obj
+        else:
+            img_data["org_img"] = img
+            img_data["crop_coor"] = (0,0)
     else:
         img = cv2.imread(img_address)
         img_data = pytesseract.image_to_data(img, output_type=Output.DICT, config=r'-l eng+heb')
         strip_char(img_data)
         img_data["img_obj"] = img
-        print(img_data)
+        if(orgImg is not None):
+            img_data["org_img"] = orgImg
+            img_data["crop_coor"] = orgImg_coor
+        # if the photo wasn't cropped, put the original image into the org_img as well as img_obj
+        else:
+            img_data["org_img"] = img
+            img_data["crop_coor"] = (0,0)
     return img_data
 
 
@@ -149,17 +185,17 @@ def MarkWords(img_sourceList, img_compareList):
 
 img_sourceList = []
 img_compareList = []
-img_compareList.append(ImageToData('C:\\OCRProj\\bigPicture.jpeg', True))
+img_compareList.append(ImageToData('C:\\OCRProj\\bigPicture.jpeg', True, 'C:\\OCRProj\\nums.png', (0,259)))
 img_sourceList.append(ImageToData('C:\\OCRProj\\green2.png', True))
 img_sourceList.append(ImageToData('C:\\OCRProj\\greentable3.png', True))
 img_sourceList.append(ImageToData('C:\\OCRProj\\sourceta.png', True))
 img_sourceList.append(ImageToData('C:\\OCRProj\\whatsappImg.png', False))
 img_sourceList.append(ImageToData('C:\\OCRProj\\whatsappImg2.png', False))
 MarkWords(img_sourceList, img_compareList)
-# cv2.imshow("example", img_data["img_obj"])
-# cv2.imshow("example2", img2_data["img_obj"])
-# cv2.imshow("example3", img3_data["img_obj"])
-# cv2.imshow("example4", img4_data["img_obj"])
+cropPaste(img_sourceList, img_compareList)
 imgShow(img_sourceList, img_compareList)
+# mybigimg = cv2.imread('C:\\OCRProj\\nums.png')
+# mybigimg[0:675, 259:361] = img_compareList[0]["img_obj"]
+# cv2.imshow("croppedimg", mybigimg)
 cv2.waitKey(0)
 
